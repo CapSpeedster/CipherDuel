63ded6c30cd73b4b5531dbeeb788fa235120bf69
Jeremy Adam <adam25j@ncssm.edu>
2025-09-12 02:36:50 -0400

    maybe render ready

--- api/index.py
+++
@@ -1,5 +0,0 @@
-from app import app  # import the Flask instance
-
-# Vercel expects a function named `handler`
-def handler(environ, start_response):
-    return app.wsgi_app(environ, start_response)
